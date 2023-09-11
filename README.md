86a12daabcabbfc1f013681d85652710c13b1581
Vadini Agarwal <vadini@Vadinis-MacBook-Air.local>
2023-09-10 21:41:51 -0400

    Add basic settings tab

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 <div align="center">
-  <h1>🪨 Obsidian Auto-Graphed</h1>
+  <h1>🪨 Obsidian AutoGraphed</h1>
   <p>An Obsidian plugin to auto-connect your notes</p>
 </div>
 
 # About this plugin
 
-- In progress 🛠️
+This is a beta version of an Obsidian plug in to automatically create connections between your notes via an LLM.
+
+You can input an API key and choose a GPT model and it will help you auto-generate connections between your notes.
+
+You have the ability to generate notes and then delete any generated notes that you don't like.
 
 # Usage
 
 **Requirement**
 
-- [Obsidian](https://obsidian.md)
-- [Git](https://git-scm.com)
-- [GitHub](https://github.com) account
-- [Node.js](https://nodejs.org)
-- Code Editor (I recommend [VSCode](https://code.visualstudio.com))
-- Basic [TypeScript](https://www.typescriptlang.org) knowledge
+-   [Obsidian](https://obsidian.md)
+-   [Git](https://git-scm.com)
+-   [GitHub](https://github.com) account
+-   [Node.js](https://nodejs.org)
+-   Code Editor (I recommend [VSCode](https://code.visualstudio.com))
+-   Basic [TypeScript](https://www.typescriptlang.org) knowledge
 
 **Installation**
 
@@ -30,5 +34,5 @@
 
 **Commands**
 
-- `npm i` (Install dependencies)
-- `npm run dev` (Install dependencies)
+-   `npm i` (Install dependencies)
+-   `npm run dev` (Install dependencies)
--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
 import axios from 'axios';
-import data from "./data.json";
 
 import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 import {preprocessAndDeduplicateNotes} from 'preprocessing';
 
-const OPENAI_API_URL = data.OPENAI_API_URL;
-const API_KEY = data.OPENAI_API_KEY;
-
 
 interface AutoGraphedSettings {
   api_key: string;
+  selected_gpt_model: string,
 }
 
 const DEFAULT_SETTINGS: AutoGraphedSettings = {
-  api_key: ''
+  api_key: '',
+  selected_gpt_model: 'gpt-3.5-turbo',
 }
 
 
@@ -62,6 +60,10 @@ export default class AutoGraphedPlugin extends Plugin {
             
             // Display connections
             console.log(connections);
+<<<<<<< HEAD
+=======
+            new Notice(`Auto-generated connections with ${this.settings.selected_gpt_model}`);
+>>>>>>> 312c06b (Add basic settings tab)
         });
 
         this.addRibbonIcon('list-x', 'Remove auto-gen connections', async () => {
@@ -89,7 +91,10 @@ export default class AutoGraphedPlugin extends Plugin {
     }
 
     onunload() {
+<<<<<<< HEAD
 		// this.addSettingTab(new AutoGraphedSettingTab(this.app, this));
+=======
+>>>>>>> 312c06b (Add basic settings tab)
     }
 
 	async loadSettings() {
@@ -105,12 +110,12 @@ export default class AutoGraphedPlugin extends Plugin {
         let promptText = "Determine the connections in keywords between the following documents. The keywords should be broader topics, like subjects or areas of study. Only list keywords they have in common.:\n\n";
 
         for (const name of fileNames) {
-            promptText += `Document ${name}: ${fileContents[name]}\n\n`;
+            promptText += `Doc ${name}: ${fileContents[name]}\n\n`;
         }
 
-        promptText += "List out your response with the following format 'Doc <document name>: keyword1, keyword2; Document: keyword1, keyword3; ...;. Be sure to end the output with a semicolon'";
+        promptText += "List out your response with the following format 'Doc <document name>: keyword1, keyword2; Doc <doc name>: keyword1, keyword3; ...;. Be sure to end the output with a semicolon'";
 
-        const gpt4Response = await this.callGPT4(promptText);
+        const gpt4Response = await this.callGPT(promptText);
 
         console.log(gpt4Response);
 
@@ -118,44 +123,120 @@ export default class AutoGraphedPlugin extends Plugin {
         const matches = gpt4Response.matchAll(/Doc(?:ument)? (.*?): (.*?);/g);
         console.log(matches);
         for (const match of matches) {
+<<<<<<< HEAD
             console.log(match);
+=======
+>>>>>>> 312c06b (Add basic settings tab)
             connections[match[1]] = match[2].split(', ').map((s: string) => s.trim());
         }
 
         return connections;
     }
 
-    async callGPT4(promptText: string): Promise<string> {
+    async callGPT(promptText: string): Promise<string> {
         try {
             const messages = [{
                 role: "user",
                 content: promptText
             }];
-    
-            const response = await axios.post(OPENAI_API_URL, {
-                model: "gpt-3.5-turbo",
+            
+            const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+                model: this.settings.selected_gpt_model,
                 messages: messages,
-                max_tokens: 400
+                max_tokens: 600
             }, {
                 headers: {
-                    'Authorization': `Bearer ${API_KEY}`,
+                    'Authorization': `Bearer ${this.settings.api_key}`,
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 }
             });
     
             if (response.data && response.data.choices && response.data.choices.length > 0) {
-                return response.data.choices[0].message.content.trim();
+                return response.data.choices[0].message.content?.trim() || '';
             } else {
                 throw new Error('No response from OpenAI.');
             }
     
         } catch (error) {
             console.error("Error calling OpenAI: ", error);
+            new Notice("Error getting connections between notes");
             throw error;
         }
     }
 
+    async test_api_key() {
+        const embed_input = "This is a test of the OpenAI API.";
+        const resp = await this.callGPT('');
+        if(resp) {
+          return true;
+        }else{
+          return false;
+        }
+    }    
+}
+
+
+class AutoGraphedSettingTab extends PluginSettingTab {
+  plugin: AutoGraphedPlugin;
+
+  constructor(app: App, plugin: AutoGraphedPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    let { containerEl } = this;
+    
+	containerEl.empty();
+
+	containerEl.createEl("h1", {
+		text: "AutoGraphed Settings"
+	});
+
+    containerEl.createEl("p", {
+		text: "This is a BETA version of AutoGraphed, an extension to automatically graph your notes."
+	});
+
+	containerEl.createEl("h2", {
+		text: "OpenAI Settings"
+	});
+
+	containerEl.createEl("p", {
+		text: "Please note that your notes are sent to OpenAI's servers to be processed and are subject to their Terms of Service"
+	});
+
+	// add a text input to enter the API key
+	new Setting(containerEl).setName("OpenAI API Key").setDesc("Required: an OpenAI API key is currently required to use AutoGraphed.").addText((text) => text.setPlaceholder("Enter your api key").setValue(this.plugin.settings.api_key).onChange(async (value) => {
+		this.plugin.settings.api_key = value.trim();
+        console.log(this.plugin.settings.api_key);
+		await this.plugin.saveSettings();
+	}));
+
+	// add a button to test the API key is working
+	new Setting(containerEl).setName("Test your API Key").setDesc("Test your API Key").addButton((button) => button.setButtonText("Test API Key").onClick(async () => {
+        // test API key
+        const resp = await this.plugin.test_api_key();
+        if (resp) {
+            new Notice("Woo! The API key is valid");
+        } else{
+            new Notice("The API key is not working as expected!");
+        }
+	}));
+
+    new Setting(containerEl)
+    .setName('Select GPT Model')
+    .setDesc('Choose your preferred GPT model for the plugin. GPT 3.5 is cheaper.')
+    .addDropdown(drop => {
+        drop.addOption('gpt-3.5-turbo', 'GPT-3.5 Turbo')
+            .addOption('gpt-4-turbo-preview', 'GPT-4 Turbo')
+            .setValue(this.plugin.settings.selected_gpt_model)
+            .onChange(async (value: string) => {
+                this.plugin.settings.selected_gpt_model = value;
+                await this.plugin.saveSettings();
+            });
+    });
+  }
 }
 
 
